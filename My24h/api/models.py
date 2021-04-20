8432d85e2b666a7fc031c3d9043d54387e12f5c9
merlou52 <61518160+merlou52@users.noreply.github.com>
2021-04-20 18:21:46 +0200

    modif db, ajout de quelques requetes

--- My24h/api/models.py
+++ My24h/api/models.py
@@ -10,7 +10,7 @@ class Race(models.Model):
 
 class Team(models.Model):
     team_name = models.CharField(max_length=50)
-    join_code = models.CharField(max_length=10)
+    join_code = models.IntegerField()
     nb_runners = models.IntegerField()
     race_type = models.ForeignKey(Race, on_delete=models.CASCADE)
 
@@ -19,10 +19,13 @@ class Runner(models.Model):
     surname = models.CharField(max_length=30)
     name = models.CharField(max_length=30)
     nickname = models.CharField(max_length=30, blank=True)
+    strava_id = models.CharField(blank=True)
+    strava_token = models.JSONField(blank=True)
     birthdate = models.DateField()
     email = models.EmailField()
-    inside_team = models.BooleanField()
-    team_name = models.ForeignKey(Team, on_delete=models.CASCADE)
+    password = models.CharField() #géré à part?
+    inside_team = models.BooleanField() # inutile peut-être
+    team_name = models.ForeignKey(Team, on_delete=models.CASCADE, blank=True)
     race_type = models.ForeignKey(Race, on_delete=models.CASCADE)
     # est-ce qu'on rajoute le nb de points par runner direct dans la db où est ce qu'on y calcule à chaque fois
     # qu'on en a besoin ?
--- My24h/api/views.py
+++ My24h/api/views.py
@@ -1,7 +1,14 @@
+import json
+from random import random
+
 from django.contrib.auth.models import User, Group
+from django.db.models import Sum
 from rest_framework import viewsets
 from rest_framework import permissions
+from rest_framework import request, status
+from rest_framework.response import Response
 from .serializer import *
+from My24h.api.models import *
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -20,3 +27,49 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+
+class SetNewRunner(request):
+    # Manque le hash du mdp, à voir si ya pas un bail déjà dans django
+    race = Race.objects.get(race_type=request.data.race_type)
+    r = Runner(name=request.data.name,
+               surname=request.data.surname,
+               birthdate=request.data.birthdate,
+               email=request.data.email,
+               inside_team=request.data.inside_team,
+               nickname=request.data.nickname,
+               race=race)  # race.id plutôt ?
+    r.save()
+    Response(json.dumps({"message": "user created"}), status=status.HTTP_201_CREATED)
+
+
+class SetTeamToRunner(request):
+    r = Runner.objects.get(id=request.data.user_id)
+    team = Team.objects.get(join_code=request.data.join_code)
+    team.nb_runners += 1  # on incrémente le nb de runner dans la team
+    r.team_name = team  # team.id plutôt ?
+    r.save()
+    team.save()
+    Response(json.dumps({"message": "Team assigned"}), status=status.HTTP_200_OK)
+
+
+class SetNewTeam(request):
+    race = Race.objects.get(race_type=request.data.race_type)
+    team_code = int(random() * 100000)
+    t = Team(name=request.data.team_name,
+             nb_runner=0,
+             join_code=team_code,
+             race=race)  # race.id plutôt ?
+    t.save()
+    Response(json.dumps({"message": "Team created", "join_code": team_code}), status=status.HTTP_201_CREATED)
+
+
+class GetRunnerTotalPoints(request):
+    km_sum = Activity.objects.aggregate(Sum('distance')).get(runner=request.data.runner_id)
+    km_points = Race.objects.get(
+        race_type=Runner.objects.get(id=request.data.runner_id).race_type).km_points  # à vérifier
+    # coeff_deniv = Race.objects.get(race_type=Runner.objects.get(id=request.data.runner_id).race_type).elevation_gain_coeff  # à vérifier
+
+    points = km_sum * km_points  # Attention, manque dénivelé
+
+    Response(json.dumps({"total_points": points}), status=status.HTTP_200_OK)

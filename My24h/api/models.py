c52c059bb399fe56b130084aac738d3e79397dba
flastare22 <francois.rlt@orange.fr>
2021-04-20 19:15:36 +0200

    [feat] Création model

--- My24h/api/models.py
+++ My24h/api/models.py
@@ -1,48 +1,37 @@
 from django.db import models
+from datetime import timedelta
+from django.contrib.auth.models import User
 
 
 class Race(models.Model):
     race_type = models.CharField(max_length=30)
-    duration = models.IntegerField()
-    km_points = models.IntegerField()
-    elevation_gain_coeff = models.FloatField()
+    duration = models.DurationField(default=timedelta(hours=24.0))
+    km_points = models.FloatField(default=0)
+    elevation_gain_coeff = models.FloatField(default=0)
 
 
 class Team(models.Model):
-    team_name = models.CharField(max_length=50)
-    join_code = models.IntegerField()
-    nb_runners = models.IntegerField()
+    name = models.CharField(max_length=50)
+    join_code = models.CharField(max_length=50, null=False, blank=False, verbose_name="Join Code")
+    nb_runners = [1, 4, 12]
     race_type = models.ForeignKey(Race, on_delete=models.CASCADE)
 
 
 class Runner(models.Model):
-    surname = models.CharField(max_length=30)
-    name = models.CharField(max_length=30)
-    nickname = models.CharField(max_length=30, blank=True)
-    strava_id = models.CharField(blank=True)
-    strava_token = models.JSONField(blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    strava_auth = models.JSONField(blank=True)
     birthdate = models.DateField()
-    email = models.EmailField()
-    password = models.CharField() #géré à part?
-    inside_team = models.BooleanField() # inutile peut-être
+    point = models.PositiveIntegerField(default=0, null=False, blank=False)
     team_name = models.ForeignKey(Team, on_delete=models.CASCADE, blank=True)
-    race_type = models.ForeignKey(Race, on_delete=models.CASCADE)
-    # est-ce qu'on rajoute le nb de points par runner direct dans la db où est ce qu'on y calcule à chaque fois
-    # qu'on en a besoin ?
 
 
 class Activity(models.Model):
-    race_type = models.ForeignKey(Race, on_delete=models.CASCADE)
+    activity_id = models.IntegerField(primary_key=True)
     runner = models.ForeignKey(Runner, on_delete=models.CASCADE)
-    team = models.ForeignKey(Team, on_delete=models.CASCADE)
-    activity_date = models.DateTimeField()
+    activity_date = models.DateTimeField(blank=False, null=False)
     upload_date = models.DateTimeField(auto_now=True)
-    distance = models.IntegerField()
-    positive_elevation_gain = models.IntegerField()
-    negative_elevation_gain = models.IntegerField()
-    run_time = models.TimeField()
+    distance = models.PositiveIntegerField(blank=False, null=False)
+    positive_elevation_gain = models.PositiveIntegerField(blank=False, null=False)
+    negative_elevation_gain = models.PositiveIntegerField(blank=False, null=False)
+    run_time = models.DurationField()
     avg_speed = models.FloatField()
-    activity_url = models.URLField()
-
-
-
